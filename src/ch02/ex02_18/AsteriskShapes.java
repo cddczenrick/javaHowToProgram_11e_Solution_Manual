b9a818a76c1404117a4da2bf3a745c9432b6c45c
cddczenrick <121370955+cddczenrick@users.noreply.github.com>
2025-04-03 01:28:43 +0800

    Create AsteriskShapes.java

---
+++ src/ch02/ex02_18/AsteriskShapes.java
@@ -0,0 +1,33 @@
+/*
+* 2.18 (Displaying Shapes with Asterisks) Write an application 
+* that displays a box, an oval, an arrow and a diamond using 
+* asterisks (*), as follows
+*
+* Author: Czenrick Mejia
+*/
+
+package ch02.ex02_18;
+
+
+public class AsteriskShapes {
+
+        public static void main (String[] args) {
+
+        //Box
+        System.out.println("********");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("******** \n");
+
+       //Circle
+        System.out.println("  ****  ");
+        System.out.println(" *    * ");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println(" *    * ");
+        System.out.println("   ***  ");     
+        }
+
+}
