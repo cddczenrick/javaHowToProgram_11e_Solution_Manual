6f44c7739d9e828f5be178cc1e2a25b1c591f460
cddczenrick <121370955+cddczenrick@users.noreply.github.com>
2025-04-02 08:37:34 +0800

    Create Arithmetic.java

---
+++ src/ch02/ex02_15/Arithmetic.java
@@ -0,0 +1,7 @@
+/**
+ * 2.15 (Arithmetic) Write an application that asks the user to enter two
+ * integers, obtains them from the user and prints their sum, product,
+ * difference and quotient (division). Use the techniques shown in Fig. 2.7.
+ * 
+ * Author: Czenrick Mejia
+ */
