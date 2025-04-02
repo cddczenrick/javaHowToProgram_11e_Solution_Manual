d9b72db39227ec21e15121a7481c70a9b3fc6b34
cddczenrick <121370955+cddczenrick@users.noreply.github.com>
2025-04-02 09:58:03 +0800

    Update ComparingIntegers.java

--- src/ch02/ex02_16/ComparingIntegers.java
+++ src/ch02/ex02_16/ComparingIntegers.java
@@ -1,1 +1,8 @@
+/*
+ *2.16 (Comparing Integers) Write an application that asks the user to enter
+ * two integers, obtains them from the user and displays whether the number 
+ and its square are greater than, equal to, not equal to, or less than the number 100.
+ *
+ * Autor: Czenrick Mejia
+ */
 
